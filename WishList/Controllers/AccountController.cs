d9c0d7afdd4cb5153862960a46929ed284d6cfa3
Tomasz Ostrozanski <tomasz.ostrozanski@westrock.com>
2023-02-22 15:36:38 +0100

    Module 4

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
+using WishList.Models.AccountViewModels;
 
 namespace WishList.Controllers
 {
@@ -20,6 +22,39 @@ namespace WishList.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var appUser = new ApplicationUser()
+                {
+                    UserName = vm.Email,
+                    Email = vm.Email
+                };
+                var Result = await _userManager.CreateAsync(appUser, vm.Password);
+                if (!Result.Succeeded)
+                {
+                    foreach(var error in Result.Errors)
+                    {
+                        ModelState.AddModelError("Password", error.Description);
+                        return View(vm);
+                    }
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(vm);
         }
     }
 }
---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
